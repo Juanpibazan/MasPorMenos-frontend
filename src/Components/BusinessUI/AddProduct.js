b5b0f703c40c38962786f1c117e4700474a824c3
JPBazan <juanpibazan3693@gmail.com>
2023-07-18 17:09:40 -0400

    added AddProduct component

--- src/Components/BusinessUI/AddProduct.js
+++ src/Components/BusinessUI/AddProduct.js
@@ -7,9 +7,33 @@ import { useStateValue } from '../../context/StateProvider';
 
 const AddProduct = ()=>{
 
-    
+    const [{place_selected}, dispatch] = useStateValue();
+    const [name,setName] = useState('');
+    const [description,setDescription] = useState('');
+    const [price,setPrice] = useState(0.0);
+
     return (
-        <div>Add Product</div>
+        <div>
+            {place_selected && (
+                <div>
+                    <h2>Agregar productos a:</h2>
+                    <h3>{place_selected.name}</h3>
+                    <div>
+                        <label>Nombre del producto</label><br/>
+                        <input type='text' value={name} onChange={(e)=>setName(e.target.value)} /><br/>
+                        <label>Description del producto</label><br/>
+                        <textarea type='text' value={description} onChange={(e)=>setDescription(e.target.value)} /><br/>
+                        <label>Precio normal</label><br/>
+                        <input type='text' value={price} onChange={(e)=>setPrice(e.target.value)} /><br/>
+                        <button>Agregar</button>
+                    </div>
+                </div>
+            )}
+
+            <div>
+
+            </div>
+        </div>
     )
 };
 
